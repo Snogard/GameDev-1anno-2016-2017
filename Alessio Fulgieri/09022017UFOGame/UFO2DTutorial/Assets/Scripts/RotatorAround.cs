4ba4e768c7a6fc6cf863960c412e007937f7dddf
Utente SoftDev1 <softdev1@comics.local>
2017-02-16 11:11:06 +0100

    il nemico adesso è fuori dal pickup e ha un prefab proprio, adesso si può scegliere la velocità del nemico dall' editor

--- Alessio Fulgieri/09022017UFOGame/UFO2DTutorial/Assets/Scripts/RotatorAround.cs
+++ Alessio Fulgieri/09022017UFOGame/UFO2DTutorial/Assets/Scripts/RotatorAround.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class RotatorAround : MonoBehaviour {
     [SerializeField]
     GameObject center;
-	
+    [SerializeField]
+    float speed=10f;
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.RotateAround(center.transform.position, Vector3.forward, 10*Time.fixedDeltaTime);
+        transform.RotateAround(center.transform.position, Vector3.forward, speed*Time.fixedDeltaTime);
 	}
 }
