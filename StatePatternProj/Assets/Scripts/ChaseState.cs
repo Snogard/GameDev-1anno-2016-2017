6f7a3074bce8133f0fb5bd1631c276104a868582
Utente SoftDev1 <softdev1@comics.local>
2017-04-12 20:53:44 +0200

    fatto il progetto per provare lo state pattern

---
+++ StatePatternProj/Assets/Scripts/ChaseState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : IState
+{
+    public void OnTriggerEnter(StateController controller)
+    {
+        
+    }
+
+    public void OnTriggerExit(StateController controller)
+    {
+        controller.ChangeState(new PatroState());
+    }
+    public void UpdateState(StateController controller)
+    {
+        controller.SetTarget(StateController.ETargetType.PLAYER);
+    }
+}
---
+++ StatePatternProj/Assets/Scripts/PatroState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatroState : IState
+{
+
+   
+
+    /// <summary>
+    /// in  pattuglia
+    /// </summary>
+    public void OnTriggerEnter(StateController controller)
+    {
+        controller.ChangeState(new ChaseState());
+    }
+
+    /// <summary>
+    /// esci pattuglia
+    /// </summary>
+    public void OnTriggerExit(StateController controller)
+    {
+        controller.SetTarget(StateController.ETargetType.WAYPOINT);
+    }
+
+    public void UpdateState(StateController controller)
+    {
+        controller.SetTarget(StateController.ETargetType.WAYPOINT);
+    }
+}
