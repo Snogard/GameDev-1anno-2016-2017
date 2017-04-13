93c398d53167650037e2ccbb5cacc197f3919e56
Utente SoftDev1 <softdev1@comics.local>
2017-04-13 10:48:32 +0200

    aggiunto il readonly

--- RogueLike/Assets/Scripts/GameManager.cs
+++ RogueLike/Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     BoardManager _boardManger;
-    [SerializeField]
+    [SerializeField][ReadOnly]
     int _level = 1;
 
     void Awake()
