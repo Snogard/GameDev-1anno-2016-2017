80ed60cc38249ded7e030f9070f37d7f7fa8aadc
Alessio Fulgieri <fulgiale@gmail.com>
2017-05-10 20:26:56 +0200

    create classi per il sandbox pattern

---
+++ DesignPattern/Assets/Sandbox/GroundDive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SanboxPattern
+{
+    public class GroundDive : Superpower
+    {
+
+        public override void Activate()
+        {
+            Move(15);
+            PlaySound("GroundDive");
+            SpawnParticles();
+        }
+    }
+}
---
+++ DesignPattern/Assets/Sandbox/Superpower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SanboxPattern
+{
+    public abstract class Superpower
+    {
+        public abstract void Activate();
+
+
+        protected void Move(float speed)
+        {
+            Debug.Log("moving " + speed);
+        }
+
+        protected void PlaySound(string coolSound)
+        {
+            Debug.Log("sound " + coolSound);
+        }
+
+        protected void SpawnParticles()
+        {
+            Debug.Log("particle");
+        }
+    }
+
+   
+}
