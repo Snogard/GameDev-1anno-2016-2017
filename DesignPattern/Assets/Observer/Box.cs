5a4c9e2bf691e430938d11afff957819d699642b
Alessio Fulgieri <fulgiale@gmail.com>
2017-05-10 19:31:01 +0200

    create alcune classi per provare il pattern observer

---
+++ DesignPattern/Assets/Observer/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ObserverPattern
+{
+    public class Box : Observer
+    {
+        GameObject boxObj;
+
+        BoxEvent boxEvent;
+
+        public Box(GameObject obj, BoxEvent boxEvent)
+        {
+            boxObj = obj;
+            this.boxEvent = boxEvent;
+        }
+
+
+        public override void OnNotify()
+        {
+            Jump(boxEvent.GetJumpForce());
+        }
+
+        void Jump(float jumpForce)
+        {
+            if(boxObj.transform.position.y<0.55f)
+            {
+                boxObj.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+            }
+        }
+    }
+}
---
+++ DesignPattern/Assets/Observer/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ObserverPattern
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject sphere;
+
+        [SerializeField]
+        GameObject box1Obj;
+        [SerializeField]
+        GameObject box2Obj;
+        [SerializeField]
+        GameObject box3Obj;
+
+        Subject subject;
+
+        // Use this for initialization
+        void Start()
+        {
+            subject = new Subject();
+
+
+            Box box1 = new Box(box1Obj, new JumpLittle());
+            Box box2 = new Box(box2Obj, new JumpMedium());
+            Box box3 = new Box(box3Obj, new JumpHigh());
+
+            subject.AddObserver(box1);
+            subject.AddObserver(box2);
+            subject.AddObserver(box3);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(sphere.transform.position.magnitude<0.5f)
+            {
+                subject.Notify();
+            }
+        }
+    }
+}
