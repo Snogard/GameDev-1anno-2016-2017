eff9691ef9847ab31a1dfe0b470e15e0958541c8
Alessio Fulgieri <fulgiale@gmail.com>
2017-05-17 19:06:28 +0200

    commentato gamecontroller

--- DesignPattern/Assets/SpacialPartitionPattern/GameController.cs
+++ DesignPattern/Assets/SpacialPartitionPattern/GameController.cs
@@ -19,13 +19,13 @@ namespace SpatialPartition
         //soildier list
         List<Soldier> enemySoldiers = new List<Soldier>();
         List<Soldier> friendlySoldiers = new List<Soldier>();
-
+        //soldati più vicini
         List<Soldier> closestEnemies = new List<Soldier>();
 
         //grid
         float mapWidth = 50f;
         int cellSize = 10;
-
+        //numero di soldati
         int numberOfSoldiers = 100;
 
         Grid grid;
@@ -36,18 +36,15 @@ namespace SpatialPartition
 
             for (int i = 0; i < numberOfSoldiers; i++)
             {
+                //creo soldato nemico
                 Vector3 randomPos = new Vector3(Random.Range(0f, mapWidth), 0.5f, Random.Range(0f, mapWidth));
-
                 GameObject newEnemy = Instantiate(enemyObj, randomPos, Quaternion.identity, enemyParent);
-
                 enemySoldiers.Add(new Enemy(newEnemy, mapWidth, grid));
 
+                //creo soldato amico
                 randomPos = new Vector3(Random.Range(0f, mapWidth), 0.5f, Random.Range(0f, mapWidth));
-
                 GameObject newFriendly = Instantiate(friendlyObj, randomPos, Quaternion.identity, friendlyParent);
-
                 friendlySoldiers.Add(new Friendly(newFriendly, mapWidth));
-
 
             }
         }
@@ -55,18 +52,19 @@ namespace SpatialPartition
         // Update is called once per frame
         void Update()
         {
+            //muovo i nemici
             for (int i = 0; i < enemySoldiers.Count; i++)
             {
                 enemySoldiers[i].Move();
             }
-
+            //assegno i maeriali
             for (int i = 0; i < closestEnemies.Count; i++)
             {
                 closestEnemies[i].soldierMeshRederer.material = enemyMaterial;
             }
-
+            
             closestEnemies.Clear();
-
+            //per ogni amico trova il nemico più vicino
             for (int i = 0; i < friendlySoldiers.Count; i++)
             {
                 Soldier closestEnemy = grid.FindClosestEnemy(friendlySoldiers[i]);
