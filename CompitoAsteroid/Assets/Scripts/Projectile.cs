6b326e5eb444fddc4622d70dd312ec49a66bb03e
Alessio Fulgieri <fulgiale@gmail.com>
2017-05-18 11:43:42 +0200

    creato proiettile (no collision)

---
+++ CompitoAsteroid/Assets/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Asteroid
+{
+    /// <summary>
+    /// class to hadle a projectile
+    /// </summary>
+    public class Projectile : MonoBehaviour
+    {
+
+        /// <summary>
+        /// the speed of the fired projectile
+        /// </summary>
+        [SerializeField]
+        private float _speed;
+        /// <summary>
+        /// the lifetime of the projectile in seconds
+        /// </summary>
+        [SerializeField]
+        private float _lifeTime;
+
+
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+            Destroy(gameObject, _lifeTime);
+        }
+
+        void FixedUpdate()
+        {
+            transform.Translate(transform.forward * _speed * Time.fixedTime);
+        }
+
+
+    }
+}
